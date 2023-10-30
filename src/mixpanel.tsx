56571de4363d183a36624dfde653d8950f4bd53b
Jack TOribio <jack.toribio@autodesk.com>
2023-10-30 10:48:07 -0600

    Remove comments

--- src/mixpanel.tsx
+++ src/mixpanel.tsx
@@ -1,25 +1,28 @@
-import mixpanel from 'mixpanel-browser';
+import mixpanel from "mixpanel-browser";
 // mixpanel.init('52d9105dc2cbd94e7b0da50a2b65f4c0');
-mixpanel.init('52d9105dc2cbd94e7b0da50a2b65f4c0', { debug: true, track_pageview: true, persistence: 'localStorage' });
+mixpanel.init("52d9105dc2cbd94e7b0da50a2b65f4c0", {
+  debug: true,
+  track_pageview: true,
+  persistence: "localStorage",
+});
 
-
-let env_check = true; // process.env.NODE_ENV === 'production';
+let env_check = true;
 
 let actions = {
-    identify: (id) => {
-        if (env_check) mixpanel.identify(id);
+  identify: (id) => {
+    if (env_check) mixpanel.identify(id);
+  },
+  alias: (id) => {
+    if (env_check) mixpanel.alias(id);
+  },
+  track: (name, props) => {
+    if (env_check) mixpanel.track(name, props);
+  },
+  people: {
+    set: (props) => {
+      if (env_check) mixpanel.people.set(props);
     },
-    alias: (id) => {
-        if (env_check) mixpanel.alias(id);
-    },
-    track: (name, props) => {
-        if (env_check) mixpanel.track(name, props);
-    },
-    people: {
-        set: (props) => {
-            if (env_check) mixpanel.people.set(props);
-        },
-    },
+  },
 };
 
 export let Mixpanel = actions;
